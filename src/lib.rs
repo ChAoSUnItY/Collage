58711b816c1bd0d566aa611a83bf2711a3c5376e
KYLE <minecraft.kyle.train@gmail.com>
2021-08-05 03:07:02 +0800

    add evaluator

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@ pub mod lexer;
 pub mod parser;
 mod utils;
 mod diagnostic;
+mod runtime;
 
 #[cfg(test)]
 mod test {
     use crate::{diagnostic::DiagnosticHolder, lexer, parser, utils};
     use test_case::test_case;
     use crate::parser::SyntaxNode;
+    use crate::runtime::Evaluator;
 
-    #[test_case("1 + 2 + 3" ; "plus expression")]
-    fn expression_parsing_test(source_code: &'static str) {
+    #[test_case("1 + 2 + 3", 6 ; "plus expression")]
+    fn expression_parsing_test(source_code: &'static str, result: i64) {
         let mut diagnostic_holder = DiagnosticHolder::new();
         let mut lexer = lexer::Lexer::new(source_code);
         let tokens = lexer.lex(&diagnostic_holder);
@@ -21,10 +23,12 @@ mod test {
         assert_eq!(ctx.len(), 1);
         assert!(ctx[0].is_some());
 
-        println!("{:?}", ctx[0].as_ref().unwrap().children());
-
         for expression in ctx.iter() {
             utils::print_syntax_tree(&Box::new(expression.to_owned()), "".to_string(), true);
         }
+
+        let evaluator = Evaluator::new(ctx.first().unwrap().clone().unwrap());
+
+        assert_eq!(evaluator.eval(), result);
     }
 }
---
+++ src/runtime.rs
@@ -0,0 +1,36 @@
+use crate::parser::Expression;
+
+pub struct Evaluator {
+    root_expression: Expression,
+}
+
+impl Evaluator {
+    pub fn new(root_expression: Expression) -> Self {
+        Self {
+            root_expression
+        }
+    }
+
+    pub fn eval(&self) -> i64 {
+        self.eval_expression(&self.root_expression)
+    }
+
+    fn eval_expression(&self, expression: &Expression) -> i64 {
+        match expression {
+            Expression::Integer(token) =>
+                token.literal.clone().parse::<i64>().unwrap(),
+            Expression::Addition(left, right) =>
+                self.eval_binary(left, right).iter().sum(),
+            Expression::Subtraction(left, right) =>
+                self.eval_binary(left, right).iter().sum(),
+            _ => panic!("Unexpected expression while evaluating.")
+        }
+    }
+
+    fn eval_binary(&self, left: &Box<Option<Expression>>, right: &Box<Option<Expression>>) -> [i64; 2] {
+        [
+            self.eval_expression(&left.clone().unwrap()),
+            self.eval_expression(&right.clone().unwrap())
+        ]
+    }
+}
