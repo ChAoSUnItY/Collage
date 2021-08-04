b1f57129e4be270109d59058f828004a53987425
KyleLin921021 <43753315+KyleLin921021@users.noreply.github.com>
2021-08-04 23:09:43 +0800

    post-commit missed files

---
+++ src/diagnostic.rs
@@ -0,0 +1,28 @@
+pub struct DiagnosticHolder {
+    diagonistic_units: Vec<Unit>
+}
+
+impl DiagnosticHolder {
+    pub fn new() -> Self {
+        Self {
+            diagonistic_units: vec![]
+        }
+    }
+
+    pub fn success(&self) -> bool {
+        self.diagonistic_units.is_empty()
+    }
+
+    pub fn warning(&mut self, message: &str) {
+        self.diagonistic_units.push(Unit::Warning(message.to_string()))
+    }
+
+    pub fn error(&mut self, message: &str) {
+        self.diagonistic_units.push(Unit::Error(message.to_string()))
+    }
+}
+
+pub enum Unit {
+    Warning(String),
+    Error(String)
+}
